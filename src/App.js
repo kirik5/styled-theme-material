810daa7879bf5b5aa2b3d1715efcbc87ee687b1a
kirik <alkirichenko81@gmail.com>
2021-10-28 16:11:45 +0300

    005 styled компонет КНОПКА + кнопки из material ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MyButton from "./MyButton";
 import {ThemeProvider} from "styled-components";
 import {primaryTheme} from "./primaryTheme";
+import {Button} from "@mui/material";
 
 function App() {
     return (
@@ -11,6 +12,9 @@ function App() {
                 <MyButton>Buy NOW!</MyButton>
                 <MyButton variant={'contained'}>Contained</MyButton>
                 <MyButton variant={'outlined'}>Outlined</MyButton>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
             </div>
         </ThemeProvider>
 
