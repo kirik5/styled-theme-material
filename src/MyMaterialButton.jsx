24b7ffa65b27f9d070481a1211e426030462bfbc
kirik <alkirichenko81@gmail.com>
2021-10-28 17:37:29 +0300

    009 создаем только свою модификация глобальной темы - все работает как и из коробки

--- src/MyMaterialButton.jsx
+++ src/MyMaterialButton.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import {styled} from '@mui/material/styles';
 import {Button} from "@mui/material";
 
-const MyMaterialButtonStyled = styled(Button)(({theme}) => ({
-    color: theme.palette.primary.main,
-    backgroundColor: theme.palette.secondary.main,
-    border: `1px solid ${theme.palette.primary}`,
-}))
+const MyMaterialButtonStyled = styled(Button)({
+
+})
 
 const MyMaterialButton = ({children, ...rest}) => {
     return <MyMaterialButtonStyled {...rest}>{children}</MyMaterialButtonStyled>
